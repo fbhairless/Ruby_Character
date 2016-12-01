3f0b9f778429ef03c19da4132f692cfcc06d9259
Hal Fairless <hbfairless@gmail.com>
2016-11-30 22:24:26 -0800

    added method rspec & method,  rspec successful

--- lib/charcount.rb
+++ lib/charcount.rb
@@ -3,7 +3,32 @@ class Charcount
   define_method(:initialize) do |phrase|
     @phrase = phrase
   end
+
   define_method(:phrase) do
     @phrase
   end
+
+ define_method(:count) do 
+    characters = []
+    total_chars = phrase.length()
+    characters = phrase.split("")
+    characters.each() do |character|
+      if (character == " " or character == "!" or character == "?" or character =="," or character == ".")
+        total_chars = total_chars.-1
+      end
+    end
+      total_chars
+  end
 end
+# #
+# phrase = "Important! Note that we will not be using Sinatra in class until Tuesday?. Monday will be spent practicing the Ruby fundamentals covered in previous pre-work sections. Begin to familiarize yourself, with Sinatra using the lessons and practice exercises in this lesson between now and Tuesday morning.."
+# characters = []
+# total_chars = phrase.length
+# characters = phrase.split("")
+# characters.each() do |character|
+#   if (character == " " or character == "!" or character == "?" or character =="," or character == ".")
+#     total_chars = total_chars.-1
+#   end
+# end
+# total_chars
+#
--- spec/charcount_spec.rb
+++ spec/charcount_spec.rb
@@ -9,10 +9,10 @@ describe(Charcount) do
     expect(in_phrase.phrase()).to(eq("hickory dickory dock"))
   end
 
-  # describe '#count' do
-  #   it 'counts the number of characters input' do
-  #     charcount = Charcount.new('hickory dickery dock')
-  #     expect(charcount.count).to eq 18
-  #   end
-  # end
+  describe '#count' do
+    it 'counts the number of characters input' do
+      charcount = Charcount.new('hickory dickery dock')
+      expect(charcount.count()).to(eq(18))
+    end
+  end
 end
